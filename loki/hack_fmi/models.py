eee6a17d95404927aa4a5a6aa242b51860d35f7e
Stanislav Bozhanov <stanislav.bozhanov@gmail.com>
2015-03-23 14:50:56 +0200

    changes to models

--- loki/hack_fmi/models.py
+++ loki/hack_fmi/models.py
@@ -17,5 +17,18 @@ class Language(models.Model):
 
 
 class Competitor(BaseUser):
+    S = 1
+    M = 2
+    L = 3
+    XL = 4
+
+    SHIRT_SIZE = (
+        (S, 'S'),
+        (M, 'M'),
+        (L, 'L'),
+        (XL, 'XL')
+    )
+
     known_technologies = models.ManyToManyField(Language)
     faculty_number = models.SmallIntegerField()
+    shirt_size = models.SmallIntegerField(choices=SHIRT_SIZE, default=S)
