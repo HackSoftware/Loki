35a8b4ba4a16de44430a7d7a694779aa4b1a8204
Stanislav Bozhanov <stanislav.bozhanov@gmail.com>
2015-03-23 13:25:15 +0200

    adding hack_fmi urls to main urls

--- loki/loki/urls.py
+++ loki/loki/urls.py
@@ -7,4 +7,5 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'', include('hack_fmi.urls', namespace='hack_fmi')),
 )
