cdfdfd1366c7f5f3e438ba964d8293a38f841fb9
Stanislav Bozhanov <stanislav.bozhanov@gmail.com>
2015-03-23 14:33:26 +0200

    models for BaseUser, Competitor and Language

--- loki/hack_fmi/models.py
+++ loki/hack_fmi/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
 
-# Create your models here.
+
+class BaseUser(AbstractBaseUser):
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    email = models.EmailField()
+    avatar = models.ImageField()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name', 'email', 'password']
+
+
+class Language(models.Model):
+    name = models.CharField(max_length=30)
+
+
+class Competitor(BaseUser):
+    known_technologies = models.ManyToManyField(Language)
+    faculty_number = models.SmallIntegerField()
