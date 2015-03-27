b7273c82f8579811aeb9f4c1109c2f68d289c5bb
Stanislav Bozhanov <stanislav.bozhanov@gmail.com>
2015-03-27 17:29:20 +0200

    Test data after login

--- loki/hack_fmi/views.py
+++ loki/hack_fmi/views.py
@@ -1,12 +1,12 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
 
 from rest_framework_jwt.views import ObtainJSONWebToken
 
-from .models import Skill,Competitor, Team
-
+from .models import Skill, Competitor, Team, TeamMembership
 from .serializers import SkillSerializer, CompetitorSerializer, TeamSerializer
 
 
@@ -18,6 +18,8 @@ class SkillListView(APIView):
 
 
 class CompetitorListView(APIView):
+    permission_classes = (IsAuthenticated,)
+
     def get(self, request, format=None):
         competitors = Competitor.objects.all()
         serializer = CompetitorSerializer(competitors, many=True)
@@ -25,6 +27,8 @@ class CompetitorListView(APIView):
 
 
 class TeamListView(APIView):
+    permission_classes = (IsAuthenticated,)
+
     def get(self, request, format=None):
         teams = Team.objects.all()
         serializer = TeamSerializer(teams, many=True)
@@ -49,15 +53,32 @@ class Login(ObtainJSONWebToken):
             user = serializer.object.get('user')
             if not user.get_competitor():
                 return Response('Not a HackFMI user.', status=status.HTTP_403_FORBIDDEN)
-
         return super(Login, self).post(request)
 
 
-# @api_view(['POST'])
-# def register_team(request):
-#     serializer = TeamSerializer(data=request.data)
+@api_view(['POST'])
+@permission_classes((IsAuthenticated,))
+def register_team(request):
+    logged_user = request.user
+    serializer = TeamSerializer(data=request.data)
+    if serializer.is_valid():
+        team = serializer.save()
+        team_membership = TeamMembership.objects.create(
+            competitor=logged_user,
+            team=team,
+            is_leader=True
+        )
+        team_membership.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+# @api_view(['GET'])
+# @permission_classes((IsAuthenticated,))
+# def get_request(request):
+#     logged_user = request.user
+#     print(logged_user)
+#     serializer = CompetitorSerializer(data=request.data)
 #     if serializer.is_valid():
-#         new_team = serializer.save()
-#         new_team.save()
-#         return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.data, status=status.HTTP_200_OK)
 #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
