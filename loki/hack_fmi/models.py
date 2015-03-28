aa21345c5f9ca47bc1d7aba7d6c2ee8a814e5fd8
Ivaylo-Bachvarov <bachvarof@gmail.com>
2015-03-28 17:51:43 +0200

    Add is_active to base user

--- loki/hack_fmi/models.py
+++ loki/hack_fmi/models.py
@@ -52,6 +52,9 @@ class BaseUser(AbstractBaseUser):
     def has_module_perms(self, app_label):
         return True
 
+    def is_active(self):
+        return True
+
     @property
     def is_staff(self):
         return self.is_admin
