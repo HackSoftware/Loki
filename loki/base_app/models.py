dc95a0f1be2d2b31f4b5ea91eda9007538f28d5f
Stanislav Bozhanov <stanislav.bozhanov@gmail.com>
2015-05-04 15:59:19 +0300

    new method for company - get_logo

--- loki/base_app/models.py
+++ loki/base_app/models.py
@@ -1,3 +1,4 @@
+from loki.local_settings import MEDIA_ROOT
 from django.db import models
 
 
@@ -5,3 +6,7 @@ class Company(models.Model):
     name = models.CharField(max_length=100, unique=True)
     logo = models.URLField(blank=True)
     jobs_link = models.URLField(blank=True)
+
+    def get_logo(self):
+        if self.logo:
+            return MEDIA_ROOT + 'logos/' + str(self.pk) + '.JPG'
