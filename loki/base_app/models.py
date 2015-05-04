5788e0f60a3eb70fa702e474ef66a399424b0a2d
Stanislav Bozhanov <stanislav.bozhanov@gmail.com>
2015-05-04 15:25:53 +0300

    Crawl command updated. Not it takes arguments to search for new companies. If no arguments are passed it searches for activaed companies in the old objects Name field unique get_logos command. Gets the logos of the companies

--- loki/base_app/models.py
+++ loki/base_app/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 
 class Company(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, unique=True)
     logo = models.URLField(blank=True)
     jobs_link = models.URLField(blank=True)
