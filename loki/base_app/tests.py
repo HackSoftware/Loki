06c965875d75a25e83b7bb99bccdbe5223b67347
Stanislav Bozhanov <stanislav.bozhanov@gmail.com>
2015-05-21 15:24:37 +0300

    Registration tests moved

--- loki/base_app/tests.py
+++ loki/base_app/tests.py
@@ -1,12 +1,14 @@
 from django.test import TestCase
 from django.core.urlresolvers import reverse
+from post_office import mail
 
 from post_office.models import EmailTemplate
+from rest_framework import status
 
 from hack_fmi.models import BaseUser
 
 
-class BaseUserTests(TestCase):
+class BaseUserRegistrationTests(TestCase):
 
     def setUp(self):
         self.user_register = EmailTemplate.objects.create(
@@ -23,7 +25,41 @@ class BaseUserTests(TestCase):
             'last_name': 'Bozhanov',
             'password': '123',
         }
-        url = reverse('base:register')
+        url = reverse('base_app:register')
         count = BaseUser.objects.count()
         self.client.post(url, data, format='json')
         self.assertEqual(count + 1, BaseUser.objects.count())
+
+    def test_register_user_no_password(self):
+        data = {
+            'email': 'ivo@abv.bg',
+            'first_name': 'Ivo',
+            'last_name': 'Bachvarov',
+        }
+        url = reverse('base_app:register')
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_email_sent(self):
+        data = {
+            'email': 'ivo@abv.bg',
+            'first_name': 'Ivo',
+            'last_name': ' Bachvarov',
+            'password': '123',
+        }
+        url = reverse('base_app:register')
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(len(mail.get_queued()), 1)
+
+    def test_email_sent_new_template(self):
+        data = {
+            'email': 'ivo@abv.bg',
+            'first_name': 'Ivo',
+            'last_name': ' Bachvarov',
+            'password': '123'
+        }
+        url = reverse('base_app:register')
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertIn(self.user_register.content, mail.get_queued()[0].message)
