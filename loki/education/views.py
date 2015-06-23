27d484a4397f60826bec0308bd1e9e964e2deeb5
Stanislav Bozhanov <stanislav.bozhanov@gmail.com>
2015-06-23 19:56:12 +0300

    Fixed import

--- loki/education/views.py
+++ loki/education/views.py
@@ -11,7 +11,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 
-from ..education.helper import crop_image
+from .helper import crop_image
 from .models import CheckIn, Student, Lecture, Course, CourseAssignment, StudentNote
 from .serializers import (UpdateStudentSerializer, StudentNameSerializer,
                           LectureSerializer, CheckInSerializer, CourseSerializer, FullCASerializer,
