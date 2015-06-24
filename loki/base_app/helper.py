38a7456db9d4ff21ff6b2ae696768170144b4c35
Stanislav Bozhanov <stanislav.bozhanov@gmail.com>
2015-06-24 17:16:52 +0300

    Chopped image is now resized to 300x300

--- loki/base_app/helper.py
+++ loki/base_app/helper.py
@@ -4,11 +4,13 @@ from django.conf import settings
 
 
 def crop_image(x1, y1, x2, y2, path):
+    size = (300, 300)
     name = settings.MEDIA_ROOT + 'avatar/' + 'cropped_' + path[2:]
     try:
         image = Image.open(os.path.join(settings.MEDIA_ROOT, path[2:]))
     except IOError:
         return False
     img = image.crop((x1, y1, y2, x2))
+    img.thumbnail(size, Image.ANTIALIAS)
     img.save(name)
     return settings.MEDIA_URL + 'avatar/' + 'cropped_' + path[2:]
