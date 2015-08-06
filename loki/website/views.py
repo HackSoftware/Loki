3dbeb8b61f461e89c83604161355d29609adc2c3
Ivaylo Bachvarof <bachvarof@gmail.com>
2015-08-06 20:11:44 +0300

    Style improvements

--- loki/website/views.py
+++ loki/website/views.py
@@ -3,6 +3,6 @@ from .models import Successor
 
 
 def index(request):
-    successors = Successor.objects.all()
+    successors = Successor.objects.order_by('?')[:6]
     success_counter = 98
     return render(request, "website/index.html", locals())
