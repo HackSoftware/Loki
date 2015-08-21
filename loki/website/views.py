b822bd52df54d9429130b82ac4657f97bd36c8f9
Ivaylo Bachvarof <bachvarof@gmail.com>
2015-08-21 19:49:30 +0300

    Add favicon and count working people

--- loki/website/views.py
+++ loki/website/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import SuccessVideo, SuccessStoryPerson, Snippet
 
+from education.models import WorkingAt
 from base_app.models import Partner
 
 
@@ -8,7 +9,9 @@ def index(request):
     successors = SuccessStoryPerson.objects.order_by('?')[:6]
     partners = Partner.objects.all().order_by('?')
     videos = SuccessVideo.objects.all()[:4]
-    success_counter = 98
+
+    success_counter = WorkingAt.objects.filter(came_working=False).count()
+
     snippets = {snippet.label: snippet for snippet in Snippet.objects.all()}
 
     return render(request, "website/index.html", locals())
