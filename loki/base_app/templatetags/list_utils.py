46510b914e511d32466d153f72123f2cecd7fdc1
Ivaylo Bachvarof <bachvarof@gmail.com>
2015-08-21 17:50:48 +0300

    Website menu action state

--- loki/base_app/templatetags/list_utils.py
+++ loki/base_app/templatetags/list_utils.py
@@ -2,15 +2,16 @@ from django import template
 
 register = template.Library()
 
+
 @register.filter
 def split_in_groups(items, n):
     try:
         n = int(n)
-    except  (ValueError, TypeError):
+    except (ValueError, TypeError):
         return [items]
 
     groups = []
-    
+
     while len(items) != 0:
         next_group = items[:n]
         groups.append(next_group)
