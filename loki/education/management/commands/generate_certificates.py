20ea4c5cce0a17ae0887b30a9f777beb496d577c
kdkocev <kkotsev94@gmail.com>
2015-08-24 18:42:53 +0300

    Adding certificates generator

--- loki/education/management/commands/generate_certificates.py
+++ loki/education/management/commands/generate_certificates.py
@@ -5,7 +5,7 @@ from education.models import Course, OldCertificate
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        courses = Course.object.find(generate_certificates_until__lt=timezone.now())
+        courses = Course.objects.filter(generate_certificates_until__lt=timezone.now())
         for course in courses:
             for assignment in course.courseassignment_set.all():
                 cert, is_new = OldCertificate.object.get_or_create(assignment=assignment)
