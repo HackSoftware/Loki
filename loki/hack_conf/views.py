e1302ac7b8b984feee8870ab1cb529b3c09b76df
kdkocev <kkotsev94@gmail.com>
2015-09-07 15:04:25 +0300

    Fixed not displaying sponsors

--- loki/hack_conf/views.py
+++ loki/hack_conf/views.py
@@ -5,14 +5,16 @@ from .models import Speaker, Sponsor, Schedule
 def home_page(request):
     speakers = Speaker.objects.all()
 
-    sponsors_silver = Sponsor.objects.filter(title=1).order_by("?")
-    sponsors_gold = Sponsor.objects.filter(title=1).order_by("?")
-    sponsors_platinum = Sponsor.objects.filter(title=1).order_by("?")
+    sponsors_silver = Sponsor.objects.filter(title=Sponsor.SPONSOR_SILVER).order_by("?")
+    sponsors_gold = Sponsor.objects.filter(title=Sponsor.SPONSOR_GOLD).order_by("?")
+    sponsors_platinum = Sponsor.objects.filter(title=Sponsor.SPONSOR_PLATINUM).order_by("?")
 
-    general_media_partners = Sponsor.objects.filter(title=2).order_by("?")
-    branch_partners = Sponsor.objects.filter(title=3).order_by("?")
-    media_partners = Sponsor.objects.filter(title=4).order_by("?")
-    school = Sponsor.objects.filter(title=5).order_by("?")
+    general_media_partners = Sponsor.objects.filter(
+        title=Sponsor.GENERAL_MEDIA_PARTNER
+    ).order_by("?")
+    branch_partners = Sponsor.objects.filter(title=Sponsor.BRANCH_PARTNER).order_by("?")
+    media_partners = Sponsor.objects.filter(title=Sponsor.MEDIA_PARTNER).order_by("?")
+    school = Sponsor.objects.filter(title=Sponsor.SCHOOL_PARTNER).order_by("?")
 
     schedule_day_one = Schedule.objects.filter(day=1).order_by("time")
     schedule_day_two = Schedule.objects.filter(day=2).order_by("time")
