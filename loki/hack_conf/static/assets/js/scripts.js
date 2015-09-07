cb6ef86e3d396d04a2a2f5f4ccdb003b2eb76f7a
Ivaylo Bachvarof <bachvarof@gmail.com>
2015-09-07 11:17:17 +0300

    Resize hackconf images

--- loki/hack_conf/static/assets/js/scripts.js
+++ loki/hack_conf/static/assets/js/scripts.js
@@ -70,27 +70,6 @@
         easing: 'swing'
     });
 
-    $('.animated').appear(function(){
-      var element = $(this);
-      var animation = element.data('animation');
-      var animationDelay = element.data('delay');
-      if (animationDelay) {
-        setTimeout(function(){
-          element.addClass( animation + " visible" );
-          element.removeClass('hiding');
-          if (element.hasClass('counter')) {
-            element.find('.value').countTo();
-          }
-        }, animationDelay);
-      }else {
-        element.addClass( animation + " visible" );
-        element.removeClass('hiding');
-        if (element.hasClass('counter')) {
-          element.find('.value').countTo();
-        }
-      }
-    },{accY: -150});
-
     $('#header').waypoint('sticky', {
         wrapper: '<div class="sticky-wrapper" />',
         stuckClass: 'sticky'
