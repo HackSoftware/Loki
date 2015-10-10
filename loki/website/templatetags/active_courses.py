f8b2965f53917a3db8a35be16463170943b0f170
PavlinGergov <gergov.pavlin@gmail.com>
2015-10-10 18:49:44 +0300

    Remove print from get_active_courses

--- loki/website/templatetags/active_courses.py
+++ loki/website/templatetags/active_courses.py
@@ -7,5 +7,4 @@ register = template.Library()
 @register.inclusion_tag('website/active_courses.html')
 def get_active_courses():
     active_courses = CourseDescription.objects.all()
-    print(active_courses)
     return {'active_courses': active_courses}
