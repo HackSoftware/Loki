b772bef80492042f13fa78171b72a16bf02d0282
Ivaylo Bachvarof <bachvarof@gmail.com>
2015-10-11 21:16:55 +0300

    Change staging IP

--- deploy_tools/config/deploy/staging.rb
+++ deploy_tools/config/deploy/staging.rb
@@ -6,7 +6,7 @@
 # server 'example.com', user: 'deploy', roles: %w{app db web}, my_property: :my_value
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}
-server 'st-data.hackbulgaria.com', user: 'hack', roles: %w{web app}, my_property: :my_value
+server '178.62.250.231', user: 'hack', roles: %w{web app}, my_property: :my_value
 
 
 
@@ -21,9 +21,9 @@ server 'st-data.hackbulgaria.com', user: 'hack', roles: %w{web app}, my_property
 # role :app, %w{deploy@example.com}, my_property: :my_value
 # role :web, %w{user1@primary.com user2@additional.com}, other_property: :other_value
 # role :db,  %w{deploy@example.com}
-role :app, %w{hack@st-data.hackbulgaria.com}
-role :web, %w{hack@st-data.hackbulgaria.com}
-role :db,  %w{hack@st-data.hackbulgaria.com}
+role :app, %w{hack@178.62.250.231}
+role :web, %w{hack@178.62.250.231}
+role :db,  %w{hack@178.62.250.231}
 
 # Configuration
 # =============
