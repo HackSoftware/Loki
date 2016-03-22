be9a56b6cb1bfdbc3364b94bfa1059c946ab7ed1
RadoRado <radorado@hackbulgaria.com>
2016-03-22 15:29:23 +0200

    Add simple management command for regrading pending solutions

---
+++ loki/education/management/commands/regrade_pending_solutions.py
@@ -0,0 +1,22 @@
+from django.core.management.base import BaseCommand
+from education.models import Solution
+from education.tasks import submit_solution
+
+
+class Command(BaseCommand):
+    help = """
+        Regrades all pending runs, in case there are stale ones
+    """
+
+    def handle(self, *args, **options):
+        pendings = Solution.objects.filter(status=Solution.PENDING)
+
+        for solution in pendings:
+            solution.status = Solution.SUBMITED
+            solution.save()
+
+            solution_id = solution.id
+
+            self.stdout.write('Regrading solution with id: {}'.format(solution_id))
+
+            submit_solution.delay(solution_id)
