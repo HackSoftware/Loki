c08bdeb5f98e6baf84b01dd308785453d69d1850
RadoRado <radorado@hackbulgaria.com>
2016-03-24 11:55:51 +0200

    Add base_app to suit admin navigation

--- loki/loki/settings.py
+++ loki/loki/settings.py
@@ -146,6 +146,7 @@ SUIT_CONFIG = {
     'MENU_OPEN_FIRST_CHILD': True,  # Default True
     'MENU_EXCLUDE': ('auth.group',),
     'MENU': (
+        {'app': 'base_app', 'icon': 'icon-user'},
         {'app': 'website', 'icon': 'icon-cog'},
         {'app': 'education', 'icon': 'icon-pencil'},
         {'app': 'hack_conf', 'icon': 'icon-briefcase'},
