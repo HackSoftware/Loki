2463fc5ce8d1938500ff1837696528eb04f3e15e
RadoRado <radorado@hackbulgaria.com>
2016-04-01 18:06:13 +0300

    Add subjects in FMI

--- loki/initial_import.sh
+++ loki/initial_import.sh
@@ -1,3 +1,4 @@
 python3 manage.py loadfixtures universities_in_bulgaria.json
 python3 manage.py loadfixtures sofia_university_faculties.json
 python3 manage.py loadfixtures academies_in_bulgaria.json
+python3 manage.py loadfixtures subjects_in_fmi.json
