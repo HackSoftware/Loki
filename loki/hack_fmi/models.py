8c2dd99fe3bd859b86d3195167410023e9d10f4e
RadoRado <radorado@hackbulgaria.com>
2016-05-02 17:03:11 +0300

    Remove hardcoded default for Season
    - add default that gives the current active season

--- loki/hack_fmi/models.py
+++ loki/hack_fmi/models.py
@@ -4,6 +4,34 @@ from ckeditor.fields import RichTextField
 from base_app.models import BaseUser
 
 
+class Season(models.Model):
+    name = models.CharField(max_length=100, null=True)
+    topic = models.CharField(max_length=100)
+    front_page = RichTextField(blank=True)
+    min_team_members_count = models.SmallIntegerField(default=1)
+    max_team_members_count = models.SmallIntegerField(default=6)
+    sign_up_deadline = models.DateField()
+    make_team_dead_line = models.DateField()
+    mentor_pick_start_date = models.DateField()
+    mentor_pick_end_date = models.DateField()
+    max_mentor_pick = models.SmallIntegerField(default=1)
+    is_active = models.BooleanField(default=False)
+
+    def save(self, *args, **kwargs):
+        if self.is_active:
+            try:
+                currently_active = self.__class__.objects.get(is_active=True)
+            except self.__class__.DoesNotExist:
+                pass
+            else:
+                currently_active.is_active = False
+                currently_active.save()
+        return super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
+
+
 class Skill(models.Model):
     name = models.CharField(max_length=30)
 
@@ -42,6 +70,11 @@ class TeamMembership(models.Model):
         return "{} {}".format(self.competitor, self.team)
 
 
+def active_season():
+    active = Season.objects.get(is_active=True)
+    return active.id
+
+
 class Team(models.Model):
     name = models.CharField(max_length=100)
     members = models.ManyToManyField('Competitor', through='TeamMembership')
@@ -49,7 +82,7 @@ class Team(models.Model):
     technologies = models.ManyToManyField('Skill', blank=True)
     idea_description = models.TextField()
     repository = models.URLField(blank=True)
-    season = models.ForeignKey('Season', default=1)
+    season = models.ForeignKey('Season', default=active_season)
     need_more_members = models.BooleanField(default=True)
     members_needed_desc = models.CharField(max_length=255, blank=True)
     room = models.ForeignKey('Room', null=True, blank=True)
@@ -75,32 +108,6 @@ class Team(models.Model):
         unique_together = (('name', 'season'),)
 
 
-class Season(models.Model):
-    name = models.CharField(max_length=100, null=True)
-    topic = models.CharField(max_length=100)
-    front_page = RichTextField(blank=True)
-    min_team_members_count = models.SmallIntegerField(default=1)
-    max_team_members_count = models.SmallIntegerField(default=6)
-    sign_up_deadline = models.DateField()
-    make_team_dead_line = models.DateField()
-    mentor_pick_start_date = models.DateField()
-    mentor_pick_end_date = models.DateField()
-    max_mentor_pick = models.SmallIntegerField(default=1)
-    is_active = models.BooleanField(default=False)
-
-    def save(self, *args, **kwargs):
-        if self.is_active:
-            try:
-                currently_active = self.__class__.objects.get(is_active=True)
-            except self.__class__.DoesNotExist:
-                pass
-            else:
-                currently_active.is_active = False
-                currently_active.save()
-        return super().save(*args, **kwargs)
-
-    def __str__(self):
-        return self.name
 
 
 class Invitation(models.Model):
