a5efb51c04479aea3127d953c5fb9026994a0afc
Ivaylo <i.donchev@hacksoft.io>
2016-05-04 12:30:24 +0300

    Reorder courses in menu

--- loki/website/templatetags/active_courses.py
+++ loki/website/templatetags/active_courses.py
@@ -6,5 +6,5 @@ register = template.Library()
 
 @register.inclusion_tag('website/active_courses.html')
 def get_active_courses():
-    active_courses = CourseDescription.objects.all()
+    active_courses = CourseDescription.objects.all().order_by('-course__start_time')
     return {'active_courses': active_courses}
