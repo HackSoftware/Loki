6d1d866b86c22cf376e0edb02addcce6b05d2aec
Krasimira Badova <krasi@hacksoft.io>
2016-06-22 12:49:31 +0300

    Initial run - Update the README, icluding the initial run of the project - Change migrations in hack_fmi app  --As we want to run the project with empty database, first we need to   load data for some Season and then migrate the other models   /check the foreign key in Team model to Season/

--- README.md
+++ README.md
@@ -1,5 +1,126 @@
-# Loki Project
-[![Build Status](https://travis-ci.org/HackSoftware/Loki.svg?branch=master)](https://travis-ci.org/HackSoftware/Loki)
-[![Coverage Status](https://coveralls.io/repos/HackSoftware/Loki/badge.svg?branch=master)](https://coveralls.io/r/HackSoftware/Loki?branch=master)
+# Loki
 
-Loki is son of Odin. That project stores all the data of HackBulgaria. It takes care of all the courses and HackFMI! It is developed by HackBulgaria Software team! 
+Loki is son of Odin. That project stores all the data of HackBulgaria.
+It takes care of all the courses and HackFMI! It is developed by HackBulgaria Software team!
+
+Local development setup
+-----------------------
+
+The project is using:
+
+-   Python 3.5+
+-   Django 1.8+
+
+In order to setup it, there are the following steps:
+
+-   Install python requirements:
+
+<!-- -->
+
+    $ pip install -r requirements.txt
+
+-   Setup Postgres
+
+<!-- -->
+
+    $ sudo -u postgres createuser your_postgres_username
+    $ sudo -u postgres createdb -O your_postgres_username hack_training
+
+-   Setup local settings
+
+<!-- -->
+
+    $ cp example_local_settings.py local_settings.py
+
+    Fill up the you local settings
+
+-   Run migrations
+
+<!-- -->
+
+    $ python manage.py makemigrations
+    $ python manage.py migrate
+
+-   Run server
+
+<!-- -->
+
+    $ python manage.py runserver
+
+-   Install npm
+
+<!-- -->
+
+    $ sudo apt-get install npm
+    & sudo apt-get install nodejs-legacy
+
+-   Install bower
+
+<!-- -->
+
+    $ sudo npm install -g bower
+
+
+
+Celery
+------
+
+This project is using Celery for async tasks such as email sending and working with Grader API.
+
+In order to run Celery, you are going to need **RabbitMQ Server**
+
+The following variables are taking care of everything, you just have to add it in our local_settings file:
+
+    BROKER_URL="amqp://guest:guest@localhost//"
+
+Afer this, you can run Celery:
+
+    $ celery -A loki worker -B
+
+Grader
+------
+
+We use [Grader](https://github.com/HackBulgaria/HackTester) in order to test the solution sent by student. In order to use the Grader API
+
+you need to set values to the following variables in the local_settings file :
+
+    GRADER_ADDRESS = "grader-address"
+    GRADER_API_KEY = "grader-api-key"
+    GRADER_API_SECRET = "your-grader-api-secret-key"
+
+
+
+Email Confirmations  -------------------
+
+In order to send confirmation emails, we must set the following variables in the local_settings file:
+EMAIL_USE_TLS = 'True/False'
+EMAIL_HOST = 'host'
+EMAIL_PORT = 'port'
+EMAIL_HOST_USER = 'host_user'
+EMAIL_HOST_PASSWORD = 'password'
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
+
+The email backend is controlled by the
+`EMAIL_BACKEND` variable, which you can set up in the settings file 
+
+
+Basic Commands
+--------------
+
+### Setting Up Your Users
+
+-   To create an **superuser account**, use this command:
+
+        $ python manage.py createsuperuser
+
+
+- 
+### Tests
+
+#### Running tests
+
+After you have installed all of the requirements, you can run the tests with:
+
+    $ python manage.py test
+
---
+++ loki/hack_fmi/migrations/0003_auto_20160622_1231.py
@@ -0,0 +1,153 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import ckeditor.fields
+import hack_fmi.models
+from django.conf import settings
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('base_app', '0014_merge'),
+        ('hack_fmi', '0002_load_season'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Competitor',
+            fields=[
+                ('baseuser_ptr', models.OneToOneField(primary_key=True, parent_link=True, to=settings.AUTH_USER_MODEL, auto_created=True, serialize=False)),
+                ('is_vegetarian', models.BooleanField(default=False)),
+                ('faculty_number', models.IntegerField(null=True)),
+                ('shirt_size', models.SmallIntegerField(default=1, choices=[(1, 'S'), (2, 'M'), (3, 'L'), (4, 'XL')])),
+                ('needs_work', models.BooleanField(default=True)),
+                ('social_links', models.TextField(blank=True)),
+                ('registered', models.BooleanField(default=False)),
+            ],
+            options={
+                'abstract': False,
+            },
+            bases=('base_app.baseuser',),
+        ),
+        migrations.CreateModel(
+            name='Invitation',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, verbose_name='ID', serialize=False)),
+                ('competitor', models.ForeignKey(to='hack_fmi.Competitor')),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Mentor',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, verbose_name='ID', serialize=False)),
+                ('name', models.CharField(max_length=100)),
+                ('description', ckeditor.fields.RichTextField()),
+                ('picture', models.ImageField(blank=True, upload_to='')),
+                ('order', models.PositiveIntegerField(default=0)),
+            ],
+            options={
+                'ordering': ('order',),
+            },
+        ),
+        migrations.CreateModel(
+            name='Partner',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, verbose_name='ID', serialize=False)),
+                ('name', models.CharField(max_length=60)),
+                ('seasons', models.ManyToManyField(to='hack_fmi.Season')),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Room',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, verbose_name='ID', serialize=False)),
+                ('number', models.IntegerField()),
+                ('capacity', models.SmallIntegerField()),
+                ('season', models.ForeignKey(to='hack_fmi.Season')),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Skill',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, verbose_name='ID', serialize=False)),
+                ('name', models.CharField(max_length=30)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Team',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, verbose_name='ID', serialize=False)),
+                ('name', models.CharField(max_length=100)),
+                ('idea_description', models.TextField()),
+                ('repository', models.URLField(blank=True)),
+                ('need_more_members', models.BooleanField(default=True)),
+                ('members_needed_desc', models.CharField(max_length=255, blank=True)),
+                ('picture', models.ImageField(blank=True, upload_to='')),
+                ('place', models.SmallIntegerField(null=True)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='TeamMembership',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, verbose_name='ID', serialize=False)),
+                ('is_leader', models.BooleanField(default=False)),
+                ('competitor', models.ForeignKey(to='hack_fmi.Competitor')),
+                ('team', models.ForeignKey(to='hack_fmi.Team')),
+            ],
+        ),
+        migrations.AddField(
+            model_name='team',
+            name='members',
+            field=models.ManyToManyField(through='hack_fmi.TeamMembership', to='hack_fmi.Competitor'),
+        ),
+        migrations.AddField(
+            model_name='team',
+            name='mentors',
+            field=models.ManyToManyField(blank=True, to='hack_fmi.Mentor'),
+        ),
+        migrations.AddField(
+            model_name='team',
+            name='room',
+            field=models.ForeignKey(blank=True, to='hack_fmi.Room', null=True),
+        ),
+        migrations.AddField(
+            model_name='team',
+            name='season',
+            field=models.ForeignKey(to='hack_fmi.Season', default=hack_fmi.models.active_season),
+        ),
+        migrations.AddField(
+            model_name='team',
+            name='technologies',
+            field=models.ManyToManyField(blank=True, to='hack_fmi.Skill'),
+        ),
+        migrations.AddField(
+            model_name='mentor',
+            name='from_company',
+            field=models.ForeignKey(null=True, to='hack_fmi.Partner'),
+        ),
+        migrations.AddField(
+            model_name='mentor',
+            name='seasons',
+            field=models.ManyToManyField(to='hack_fmi.Season'),
+        ),
+        migrations.AddField(
+            model_name='invitation',
+            name='team',
+            field=models.ForeignKey(to='hack_fmi.Team'),
+        ),
+        migrations.AddField(
+            model_name='competitor',
+            name='known_skills',
+            field=models.ManyToManyField(to='hack_fmi.Skill'),
+        ),
+        migrations.AlterUniqueTogether(
+            name='team',
+            unique_together=set([('name', 'season')]),
+        ),
+        migrations.AlterUniqueTogether(
+            name='invitation',
+            unique_together=set([('team', 'competitor')]),
+        ),
+    ]
