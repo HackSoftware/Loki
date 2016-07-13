8850ee9140dd766503d466cc345a7eb0452936dd
Krasimira Badova <krasi@hacksoft.io>
2016-07-13 11:31:45 +0300

    Add apps.py & signal folder

---
+++ loki/hack_fmi/apps.py
@@ -0,0 +1,12 @@
+from django.apps import AppConfig
+
+
+class HackFMIConfig(AppConfig):
+
+    name = 'hack_fmi'
+    verbose_name = 'Hack FMI'
+
+    def ready(self):
+
+        # import signal handlers
+        import hack_fmi.signals.handlers
