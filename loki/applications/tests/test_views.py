ca26c7ad3b501c7f2fba2e7a27ba2d38f08af297
Radoslav Georgiev <radorado@hackbulgaria.com>
2016-08-21 21:18:36 +0300

    Add tests/ folder with sime view tests

---
+++ loki/applications/tests/test_views.py
@@ -0,0 +1,20 @@
+from datetime import timedelta
+
+from django.utils import timezone
+
+from test_plus.test import TestCase
+
+from seed.factories import faker, CourseFactory
+from ..models import ApplicationInfo
+
+
+class TestApplicationViews(TestCase):
+    def setUp(self):
+        self.course = CourseFactory()
+        self.application_info = ApplicationInfo.objects.create(course=self.course,
+                                                               start_date=timezone.now(),
+                                                               end_date=timezone.now() + timedelta(1))
+
+    def test_non_registered_user_cannot_apply(self):
+        self.get('website:apply_course', course_url=self.course.url)
+        self.response_302()
