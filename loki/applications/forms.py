8ff02bb4434d6d368f5e1565f0d4ba8a47248833
Rositsazz <ross_zz@mail.bg>
2016-08-30 22:56:50 +0300

    Try to save apply edit form

--- loki/applications/forms.py
+++ loki/applications/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 from website.forms import w
+from .models import (Application, ApplicationInfo,
+                     ApplicationProblem, ApplicationProblemSolution)
 from django.utils.translation import ugettext_lazy as _
 
 
@@ -21,3 +23,19 @@ class ApplyForm(forms.Form):
             self.fields['task_{index}'.format(index=index+1)] = \
                 forms.URLField(label=task_label,
                                widget=w('text', 'Решение на задача'))
+
+    def save(self, app_info, app_problems, user):
+        application = Application.objects.create(
+            user=user,
+            application_info=app_info,
+            phone=self.cleaned_data.get('phone'),
+            skype=self.cleaned_data.get('skype'),
+            works_at=self.cleaned_data.get('works_at'),
+            studies_at=self.cleaned_data.get('studies_at'))
+
+        for index, app_problem in enumerate(app_problems):
+            ApplicationProblemSolution.objects.create(
+                application=application,
+                problem=app_problem,
+                solution_url=self.cleaned_data.get('task_{0}'.format(index+1))
+            )
