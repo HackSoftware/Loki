b5a6c707770c4841457e9048d0175bef06a122be
Radoslav Georgiev <radorado@hackbulgaria.com>
2016-09-01 14:51:37 +0300

    Remove hack_conf from Loki

--- loki/hack_conf/static/assets/js/form-validation.js
+++
@@ -1,5 +0,0 @@
-// Form validation - register form
-	toastr.options = {"positionClass": "toast-top-full-width"};
-    $('.form-register').validate({
-        email: "Please enter a valid email address"
-    });
