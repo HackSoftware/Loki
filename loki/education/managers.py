8e3e6f92fc2c5772f2e8edcd7829815c89a51974
Radoslav Georgiev <radorado@hackbulgaria.com>
2016-09-05 19:40:13 +0300

    Add more test cases
    - Don't create if already a student

--- loki/education/managers.py
+++ loki/education/managers.py
@@ -3,8 +3,12 @@ from django.db import models
 
 class StudentManager(models.Manager):
     def create_from_baseuser(self, baseuser):
+        if baseuser.get_student() is not False:
+            return None
+
         student = self.model(baseuser_ptr_id=baseuser.id)
-        student.save()
 
         student.__dict__.update(baseuser.__dict__)
-        return student.save()
+        student.save()
+
+        return student
