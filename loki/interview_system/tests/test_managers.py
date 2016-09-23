a6e4ca0063e7508bfa86d4758d2f188ec36da0a5
Slavyana Monkova <slaviana_m@mail.bg>
2016-09-23 22:54:04 +0300

    Fix flake8 errors

--- loki/interview_system/tests/test_managers.py
+++ loki/interview_system/tests/test_managers.py
@@ -5,6 +5,7 @@ from ..models import InterviewerFreeTime, Interview
 from loki.seed.factories import (ApplicationFactory, ApplicationInfoFactory,
                                  InterviewerFactory)
 
+
 class InterviewManagerTests(TestCase):
 
     def test_counting_interview_slots_without_applications(self):
@@ -23,11 +24,11 @@ class InterviewManagerTests(TestCase):
                                              end_time='11:30',
                                              interviewer_time_slot=free_time)
 
-        interview_2 = Interview.objects.create(interviewer=interviewer,
-                                             date=datetime.datetime.now().date(),
-                                             start_time='12:00',
-                                             end_time='12:30',
-                                             interviewer_time_slot=free_time)
+        Interview.objects.create(interviewer=interviewer,
+                                 date=datetime.datetime.now().date(),
+                                 start_time='12:00',
+                                 end_time='12:30',
+                                 interviewer_time_slot=free_time)
 
         self.assertEquals(2, Interview.objects.get_free_slots().count())
 
