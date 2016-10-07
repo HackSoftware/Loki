b505bba05092715414c9724dd0bea95a999318eb
RadoRado <radorado@hackbulgaria.com>
2016-10-07 13:30:17 +0300

    Import celery in app's `__init__.py`
    - Uncomment failing test due to celery not taking the right settings

--- loki/interview_system/tests/test_send_email.py
+++ loki/interview_system/tests/test_send_email.py
@@ -13,29 +13,28 @@ from ..models import InterviewerFreeTime, Interview
 
 class SendEmailForInterviewsTests(TestCase):
     def test_send_email_for_interview_correctly(self):
-        pass
-        # app_info = ApplicationInfoFactory()
-        # application = ApplicationFactory(application_info=app_info)
-        # interviewer = InterviewerFactory()
-        #
-        # interviewer.courses_to_interview.add(app_info)
-        #
-        # InterviewerFreeTime.objects.create(interviewer=interviewer,
-        #                                    date=datetime.datetime.now().date(),
-        #                                    start_time='11:00',
-        #                                    end_time='14:00')
-        #
-        # self.assertEqual(0, Interview.objects.count())
-        # self.assertFalse(application.has_interview_date)
-        # self.assertEquals(1, Application.objects.without_interviews().count())
-        #
-        # call_command('generate_interview_slots')
-        #
-        # application.refresh_from_db()
-        #
-        # self.assertEquals(0, Application.objects.without_interviews().count())
-        # self.assertTrue(application.has_interview_date)
-        #
-        # call_command('send_emails_for_interviews')
-        #
-        # self.assertEqual(len(mail.outbox), 1)
+        app_info = ApplicationInfoFactory()
+        application = ApplicationFactory(application_info=app_info)
+        interviewer = InterviewerFactory()
+
+        interviewer.courses_to_interview.add(app_info)
+
+        InterviewerFreeTime.objects.create(interviewer=interviewer,
+                                           date=datetime.datetime.now().date(),
+                                           start_time='11:00',
+                                           end_time='14:00')
+
+        self.assertEqual(0, Interview.objects.count())
+        self.assertFalse(application.has_interview_date)
+        self.assertEquals(1, Application.objects.without_interviews().count())
+
+        call_command('generate_interview_slots')
+
+        application.refresh_from_db()
+
+        self.assertEquals(0, Application.objects.without_interviews().count())
+        self.assertTrue(application.has_interview_date)
+
+        call_command('send_emails_for_interviews')
+
+        self.assertEqual(len(mail.outbox), 1)
