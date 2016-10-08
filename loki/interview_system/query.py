f94a22b843929613b84c314689e8b88ece4efa0d
Slavyana Monkova <slaviana_m@mail.bg>
2016-10-08 19:27:41 +0300

    NOT ordering by date

--- loki/interview_system/query.py
+++ loki/interview_system/query.py
@@ -7,8 +7,8 @@ class InterviewQuerySet(models.QuerySet):
         return self.filter(application__isnull=True).order_by('date', 'start_time')
 
     def free_slots_for(self, application_info):
-        return self.get_free_slots().filter(
-               interviewer__courses_to_interview__in=[application_info])
+        return self.filter(application__isnull=True).filter(
+                interviewer__courses_to_interview__in=[application_info])
 
     def with_application(self):
         return self.filter(application__isnull=False)
