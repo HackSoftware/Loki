7896018c82ad5b74c4aa3287afa46d1925eb1ac5
Rositsazz <ross_zz@mail.bg>
2016-11-03 11:47:51 +0200

    Test for task list view

--- loki/education/tests/test_views.py
+++ loki/education/tests/test_views.py
@@ -1,6 +1,7 @@
 from test_plus.test import TestCase
 
-from loki.seed.factories import BaseUserFactory
+from loki.seed.factories import (BaseUserFactory, CourseFactory, TaskFactory,
+                                 CourseAssignmentFactory)
 from loki.base_app.models import BaseUser
 
 
@@ -26,3 +27,42 @@ class CourseListViewTests(TestCase):
         with self.login(username=student.email, password=BaseUserFactory.password):
             response = self.get('education:course_list')
             self.assertEqual(response.status_code, 200)
+
+
+class TaskViewTests(TestCase):
+
+    def setUp(self):
+        self.baseuser = BaseUserFactory()
+        self.baseuser.is_active = True
+        self.baseuser.save()
+        self.baseuser2 = BaseUserFactory()
+        self.baseuser2.is_active = True
+        self.baseuser2.save()
+        self.student = BaseUser.objects.promote_to_student(self.baseuser2)
+        self.course = CourseFactory()
+        self.task = TaskFactory(course=self.course)
+        self.course_assignment = CourseAssignmentFactory(course=self.course,
+                                                         user=self.student)
+
+    def test_student_access_task_list(self):
+        with self.login(email=self.student.email, password=BaseUserFactory.password):
+            response = self.get('education:course_dashboard', course=self.course.id)
+            self.assertEqual(response.status_code, 200)
+
+    def test_baseuser_cannot_access_task_list(self):
+        with self.login(email=self.baseuser.email, password=BaseUserFactory.password):
+            response = self.get('education:course_dashboard', course=self.course.id)
+            self.assertEqual(response.status_code, 403)
+
+    def test_student_cannot_access_task_list_of_course_without_tasks(self):
+        course2 = CourseFactory()
+        CourseAssignmentFactory(course=course2, user=self.student)
+        with self.login(email=self.student.email, password=BaseUserFactory.password):
+            response = self.get('education:course_dashboard', course=course2.id)
+            self.assertEqual(response.status_code, 404)
+
+    def test_baseuser_cannot_access_task_list_of_course_without_tasks(self):
+        course2 = CourseFactory()
+        with self.login(email=self.baseuser.email, password=BaseUserFactory.password):
+            response = self.get('education:course_dashboard', course=course2.id)
+            self.assertEqual(response.status_code, 403)
