4d897c71c3aef4888ba29e9227ae61c181f49cde
Slavyana Monkova <slaviana_m@mail.bg>
2016-11-03 18:29:39 +0200

    Rename templatetag file

--- loki/education/templatetags/n.py
+++
@@ -1,8 +0,0 @@
-from django import template
-from django.utils import timezone
-
-register = template.Library()
-
-@register.filter
-def is_date_in_future(date_field):
-    return date_field > timezone.now().date()
