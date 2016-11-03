172cca7b459aebd6da7639eeea96a5d09fe60685
Slavyana Monkova <slaviana_m@mail.bg>
2016-11-03 18:31:32 +0200

    New file for templatetag

---
+++ loki/education/templatetags/check_date.py
@@ -0,0 +1,8 @@
+from django import template
+from django.utils import timezone
+
+register = template.Library()
+
+@register.filter
+def is_date_in_future(date_field):
+    return date_field > timezone.now().date()
