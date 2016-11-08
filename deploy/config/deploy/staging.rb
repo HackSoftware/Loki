4cd43f16d5ed0d4cb3dacb508e259de5c22492f8
Ivaylo Bachvarof <bachvarof@gmail.com>
2016-11-08 11:11:14 +0200

    Change loki staging and production domains in cap

--- deploy/config/deploy/staging.rb
+++ deploy/config/deploy/staging.rb
@@ -6,7 +6,7 @@
 # server 'example.com', user: 'deploy', roles: %w{app db web}, my_property: :my_value
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}
-server 'staging.data.hackbulgaria.com', user: 'hack', roles: %w{web app}, my_property: :my_value
+server 'staging.hackbulgaria.com', user: 'hack', roles: %w{web app}, my_property: :my_value
 
 
 
