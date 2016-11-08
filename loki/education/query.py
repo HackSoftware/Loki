aea6152782eec669775586cb436c9a01a8e5b2e5
Rositsazz <ross_zz@mail.bg>
2016-11-08 11:29:30 +0200

    Check if user is course assignment

--- loki/education/query.py
+++ loki/education/query.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 
 class SolutionQuerySet(models.QuerySet):
@@ -14,3 +15,12 @@ class CheckInQuerySet(models.QuerySet):
                            date__gte=course.start_time,
                            date__lte=course.end_time).values_list(
                                               'date', flat=True)
+
+
+class CourseQuerySet(models.QuerySet):
+
+    def get_active_courses(self):
+        return self.filter(end_time__gte=timezone.now().date())
+
+    def get_closed_courses(self):
+        return self.filter(end_time__lte=timezone.now().date())
