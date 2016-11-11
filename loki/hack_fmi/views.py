95af55effcb8216d82ebdda49dc07f85ed77e4da
Slavyana Monkova <slaviana_m@mail.bg>
2016-11-11 12:35:07 +0200

    fix responses of onboarding api

--- loki/hack_fmi/views.py
+++ loki/hack_fmi/views.py
@@ -211,8 +211,10 @@ class OnBoardCompetitor(APIView):
             if serializer.is_valid():
                 serializer.save()
                 return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        return Response("User is already competitor!")
 
 class TestApi(APIView):
     permission_classes = (IsAuthenticated,)
