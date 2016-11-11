70809444368b6f7b313dfde654704bd82b911b99
Slavyana Monkova <slaviana_m@mail.bg>
2016-11-11 10:08:13 +0200

    Moving views for student in student_dashboard.py

--- loki/education/urls.py
+++ loki/education/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
 
-from loki.education.views import old_api_views, dashboard
+from loki.education.views import old_api_views, student_dashboard
 
 
 urlpatterns = [
@@ -24,11 +24,11 @@ urlpatterns = [
     url(r'^api/student-solutions/$', old_api_views.StudentSolutionsList.as_view(), name='student_solutions'),
     url(r'^certificate/(?P<pk>[0-9]+)/$', old_api_views.certificate_old, name='certificate_old'),
     url(r'^certificate/(?P<token>[\w|-]+)/$', old_api_views.certificate, name='certificate'),
-    url(r'^dashboard/$', dashboard.CourseListView.as_view(), name='course_list'),
+    url(r'^dashboard/$', student_dashboard.CourseListView.as_view(), name='course_list'),
     url(r'^course/(?P<course>[0-9]+)/dashboard/$',
-        dashboard.CourseDashboardView.as_view(), name='course_dashboard'),
+        student_dashboard.CourseDashboardView.as_view(), name='course_dashboard'),
     url(r'^course/(?P<course>[0-9]+)/task/(?P<task>[0-9]+)/solutions/$',
-        dashboard.SolutionView.as_view(), name='solution_view'),
+        student_dashboard.SolutionView.as_view(), name='solution_view'),
     url(r'^course/(?P<course>[0-9]+)/materials/$',
-        dashboard.MaterialView.as_view(), name='material_view')
+        student_dashboard.MaterialView.as_view(), name='material_view')
 ]
