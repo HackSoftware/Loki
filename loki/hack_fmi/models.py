4d6e5b392e960996e3784adac63dd8b3b96cea22
Slavyana Monkova <slaviana_m@mail.bg>
2016-11-21 18:11:53 +0200

    Delete picture field of Team model

--- loki/hack_fmi/models.py
+++ loki/hack_fmi/models.py
@@ -122,7 +122,6 @@ class Team(models.Model):
     need_more_members = models.BooleanField(default=True)
     members_needed_desc = models.CharField(max_length=255, blank=True)
     room = models.ForeignKey('Room', null=True, blank=True)
-    picture = models.ImageField(blank=True)
     place = models.SmallIntegerField(blank=True, null=True)
 
     objects = TeamQuerySet.as_manager()
