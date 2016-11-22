cf1aa1ff21394acce1c5c8f06ae3609b640f4fa6
Slavyana Monkova <slaviana_m@mail.bg>
2016-11-22 19:12:34 +0200

    Using utils.make_mock_object to create request everywhere

--- loki/hack_fmi/tests/test_permissions.py
+++ loki/hack_fmi/tests/test_permissions.py
@@ -1,5 +1,5 @@
 from test_plus.test import TestCase
-from loki.hack_fmi.models import Team, TeamMembership, Competitor, Invitation, TeamMentorship
+from loki.common.utils import make_mock_object
 from loki.seed.factories import (BaseUserFactory, CompetitorFactory, TeamFactory,
                                  TeamMembershipFactory)
 from loki.hack_fmi.permissions import IsHackFMIUser, IsTeamLeaderOrReadOnly
@@ -11,12 +11,12 @@ class HackFMIUserTest(TestCase):
 
     def test_non_hackfmi_user_can_not_access(self):
         user = BaseUserFactory()
-        request = type('', (object, ), {'user': user})
+        request = make_mock_object(user=user)
         self.assertFalse(self.permission.has_permission(request, view=None))
 
     def test_hackfmi_user_can__access(self):
         user = CompetitorFactory()
-        request = type('', (object, ), {'user': user})
+        request = make_mock_object(user=user)
         self.assertTrue(self.permission.has_permission(request, view=None))
 
 class IsTeamLeaderOrReadOnlyTest(TestCase):
@@ -24,41 +24,41 @@ class IsTeamLeaderOrReadOnlyTest(TestCase):
         self.permission = IsTeamLeaderOrReadOnly()
         self.team = TeamFactory()
         self.competitor = CompetitorFactory()
-        self.team_membership = TeamMembership(competitor=self.competitor,
-                               team=self.team,
-                               is_leader=False)
+        self.team_membership = TeamMembershipFactory(competitor=self.competitor,
+                                                     team=self.team,
+                                                     is_leader=False)
 
     def test_non_team_leader_can_read(self):
-        request = type('', (object, ), {'user': self.competitor, 'method': 'GET'})
+        request = make_mock_object(user=self.competitor, method='GET')
         self.assertTrue(self.permission.has_object_permission(request, view=None, obj=self.team))
 
     def test_non_team_leader_can_not_delete_team(self):
-        request = type('', (object, ), {'user': self.competitor, 'method': 'DELETE'})
+        request = make_mock_object(user=self.competitor, method='DELETE')
         self.assertFalse(self.permission.has_object_permission(request, view=None, obj=self.team))
 
     def test_non_team_leader_can_not_update_team(self):
-        request = type('', (object, ), {'user': self.competitor, 'method': 'PATCH'})
+        request = make_mock_object(user=self.competitor, method='PATCH')
         self.assertFalse(self.permission.has_object_permission(request, view=None, obj=self.team))
 
     def test_team_leader_can_read(self):
         self.team_membership.is_leader = True
         self.team_membership.save()
 
-        request = type('', (object, ), {'user': self.competitor, 'method': 'GET'})
+        request = make_mock_object(user=self.competitor, method='GET')
         self.assertTrue(self.permission.has_object_permission(request, view=None, obj=self.team))
 
     def test_team_leader_can_delete_team(self):
         self.team_membership.is_leader = True
         self.team_membership.save()
 
-        request = type('', (object, ), {'user': self.competitor, 'method': 'DELETE'})
+        request = make_mock_object(user=self.competitor, method='DELETE')
         self.assertTrue(self.permission.has_object_permission(request, view=None, obj=self.team))
 
     def test_team_leader_can_update_team(self):
         self.team_membership.is_leader = True
         self.team_membership.save()
 
-        request = type('', (object, ), {'user': self.competitor, 'method': 'PATCH'})
+        request = make_mock_object(user=self.competitor, method='PATCH')
         self.assertTrue(self.permission.has_object_permission(request, view=None, obj=self.team))
 
 if __name__ == '__main__':
