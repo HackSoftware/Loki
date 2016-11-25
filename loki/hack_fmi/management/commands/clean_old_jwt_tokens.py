9a3fa0f04fe76ffe6b48a01903748fc287b487f0
Krasimira Badova <krasi@hacksoft.io>
2016-11-25 11:56:56 +0200

    Import JWT_AUTH expiration days from settings

--- loki/hack_fmi/management/commands/clean_old_jwt_tokens.py
+++ loki/hack_fmi/management/commands/clean_old_jwt_tokens.py
@@ -1,4 +1,5 @@
 from django.core.management.base import BaseCommand
+from django.conf import settings
 from loki.hack_fmi.models import BlackListToken
 from ...helper import get_date_with_timedelta
 
@@ -7,6 +8,7 @@ class Command(BaseCommand):
     help = 'Clean jwt tokens which are older than 1 week'
 
     def handle(self, *args, **options):
-        older_than_one_week = get_date_with_timedelta(days=-7)
-        BlackListToken.objects.filter(created_at__lte=older_than_one_week).delete()
+        days = settings.JWT_AUTH['JWT_EXPIRATION_DELTA'].days
+        blacklisted_dates = get_date_with_timedelta(days=-days)
+        BlackListToken.objects.filter(created_at__lte=blacklisted_dates).delete()
         print("BlackListTokens deleted.")
