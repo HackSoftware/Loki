50e3be4c100128fddfc6fa9675aeef6041a4652f
martin95056 <martinangelov056@gmail.com>
2016-11-28 14:35:50 +0200

    Fix bug in SeasonCompetitorManager.

--- loki/hack_fmi/managers.py
+++ loki/hack_fmi/managers.py
@@ -42,4 +42,4 @@ class SeasonCompetitorInfoManager(models.Manager):
         return SeasonCompetitorInfoQuerySet(self.model, using=self._db)
 
     def get_competitors_for_active_season(self):
-        return [s.competitor for s in self.get_queryset().get_season_competitor_infos_for_active_season()]
+        return [s.is_competitor_leaderor for s in self.get_queryset().get_season_competitor_infos_for_active_season()]
