0ce92229e18a4137ee3f3b5ab1f039ea7487811f
Pavlin Gergov <gergov.pavlin@gmail.com>
2016-11-30 16:23:46 +0200

    Introduce generic routing

--- config/routing.py
+++ config/routing.py
@@ -1,8 +1,6 @@
-from channels.routing import route_class
-from loki.hack_fmi import consumers
+from channels import include
 
 
 channel_routing = [
-    # ws://localhost:8000/invitations
-    route_class(consumers.InvitationServer, path=r'^/invitations')
+    include('loki.hack_fmi.routing.hackfmi_routing'),
 ]
