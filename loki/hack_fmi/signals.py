1211b01f8d081982a620d93e6a9dce4448c6485c
Pavlin Gergov <gergov.pavlin@gmail.com>
2016-12-01 13:40:56 +0200

    Fix competitor's season info update in signal
    * Update only when team membership is created, but not when updated
    * Add check if competitor's season info exists

--- loki/hack_fmi/signals.py
+++ loki/hack_fmi/signals.py
@@ -5,7 +5,13 @@ from .models import TeamMembership
 
 
 @receiver(post_save, sender=TeamMembership)
-def set_looking_for_team_to_false(sender, instance, **kwargs):
-    sci_object = instance.competitor.seasoncompetitorinfo_set.first()
-    sci_object.looking_for_team = False
-    sci_object.save()
+def set_looking_for_team_to_false(sender, instance, created, **kwargs):
+    if created:
+        """
+        We have unique_together = ('competitor', 'season')
+        so the query will return one object or None.
+        """
+        season_competitor_info = instance.competitor.seasoncompetitorinfo_set.first()
+        if season_competitor_info:
+            season_competitor_info.looking_for_team = False
+            season_competitor_info.save()
