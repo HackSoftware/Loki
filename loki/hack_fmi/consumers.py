5888a827b95239828ad42f634d7671419542c859
Pavlin Gergov <gergov.pavlin@gmail.com>
2016-12-01 13:00:20 +0200

    Add receive method for handshake

--- loki/hack_fmi/consumers.py
+++ loki/hack_fmi/consumers.py
@@ -7,5 +7,8 @@ class InvitationConsumer(WebsocketConsumer):
     def connect(self, message, **kwargs):
         print("connect")
 
+    def receive(self, text=None, bytes=None, **kwargs):
+        print(text)
+
     def disconnect(self, message, **kwargs):
         print("disconnect")
