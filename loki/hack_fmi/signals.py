a2a8cf48ee8df153e39cb3fbc06848f1f90dd37f
Krasimira Badova <krasi@hacksoft.io>
2016-12-05 17:21:25 +0200

    Send message to group in send_message_to_group if Invitation is created

--- loki/hack_fmi/signals.py
+++ loki/hack_fmi/signals.py
@@ -10,20 +10,20 @@ from .models import Invitation, TeamMembership, Season, SeasonCompetitorInfo, Co
 
 
 @receiver(post_save, sender=Invitation)
-def send_message_to_group(sender, instance, **kwargs):
-    # TODO: Broadcast only when instance is created?
-    msg = "New invitation was created."
-    leader_name = instance.team.get_leader().full_name
-    competitor_id = instance.competitor.id
+def send_message_to_group(sender, instance, created, **kwargs):
+    if created:
+        msg = "New invitation was created."
+        leader_name = instance.team.get_leader().full_name
+        competitor_id = instance.competitor.id
 
-    data = {
-        'message': msg,
-        'leader': leader_name,
-        'competitor_id': competitor_id
-    }
+        data = {
+            'message': msg,
+            'leader': leader_name,
+            'competitor_id': competitor_id
+        }
 
-    group_name = settings.INVITATION_GROUP_NAME.format(id=instance.competitor.id)
-    Group(group_name).send({"text": json.dumps(data)})
+        group_name = settings.INVITATION_GROUP_NAME.format(id=instance.competitor.id)
+        Group(group_name).send({"text": json.dumps(data)})
 
 
 @receiver(post_save, sender=TeamMembership)
