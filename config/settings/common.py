541f4e3bb857b55714d8f32c7e0414af2b794f9d
Pavlin Gergov <gergov.pavlin@gmail.com>
2016-12-08 17:07:46 +0200

    Add channels to installed apps

--- config/settings/common.py
+++ config/settings/common.py
@@ -53,6 +53,7 @@ INSTALLED_APPS = (
     'anymail',
     'easy_thumbnails',
     'image_cropping',
+    'channels',
     'raven.contrib.django.raven_compat',  # used for sentry logging
 
     'loki.hack_fmi.apps.HackFMIConfig',
