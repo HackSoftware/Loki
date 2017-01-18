6e1a17edaea87106f9d37fc2ac60a273e8d421a7
Slavyana Monkova <slaviana_m@mail.bg>
2017-01-18 10:34:10 +0200

    Using new service in view and remove checks

--- loki/education/views/dashboard.py
+++ loki/education/views/dashboard.py
@@ -4,7 +4,7 @@ from loki.education.models import Course, Material, CheckIn
 from loki.education.mixins import (DashboardPermissionMixin,
                                    CannotSeeOthersCoursesDashboardsMixin)
 from loki.education.helper import get_dates_for_weeks, percentage_presence
-
+from loki.education.services import get_course_presence
 
 class CourseListView(DashboardPermissionMixin,
                      TemplateView):
@@ -13,49 +13,31 @@ class CourseListView(DashboardPermissionMixin,
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+
         if self.request.user.get_teacher():
-            context['t_courses'] = self.request.user.teacher.teached_courses.all()
-        else:
-            context['t_courses'] = None
+            context['teacher_courses'] = self.request.user.teacher.teached_courses.all()
+
         if self.request.user.get_student():
-            context['st_courses'] = Course.objects.filter(
+            context['student_courses'] = Course.objects.filter(
                 courseassignment__user=self.request.user).order_by('-end_time')
-        else:
-            context['st_courses'] = None
-        if self.request.user.get_student():
-            st_user = self.request.user.student
-        if self.request.user.get_teacher():
-            t_user = self.request.user.teacher
 
-        t_course_presence = {}
-        if context['t_courses']:
-            for course in context['t_courses']:
+        user = self.request.user.get_student() if self.request.user.get_student() else self.request.get_teacher()
+
+        course_presence = {}
+        if context['teacher_courses'].exists():
+            for course in context['teacher_courses']:
                 if not course.lecture_set.exists():
                     continue
-                t_course_presence[course] = {}
-                t_course_presence[course]['weeks'] = list(get_dates_for_weeks(course).keys())
-                t_course_presence[course]['dates_for_weeks'] = get_dates_for_weeks(course)
-                t_course_presence[course]['user_dates'] = CheckIn.objects.get_user_dates(t_user, course)
+                course_presence[course] = get_course_presence(course=course, user=user)
 
-                user_dates = t_course_presence[course]['user_dates']
-                t_course_presence[course]['percentage_presence'] = percentage_presence(user_dates, course)
 
-        context['t_course_presence'] = t_course_presence
-
-        st_course_presence = {}
-        if context['st_courses']:
-            for course in context['st_courses']:
+        if context['student_courses'].exists():
+            for course in context['student_courses']:
                 if not course.lecture_set.exists():
                     continue
-                st_course_presence[course] = {}
-                st_course_presence[course]['weeks'] = list(get_dates_for_weeks(course).keys())
-                st_course_presence[course]['dates_for_weeks'] = get_dates_for_weeks(course)
-                st_course_presence[course]['user_dates'] = CheckIn.objects.get_user_dates(st_user, course)
+                course_presence[course] = get_course_presence(course=course, user=user)
 
-                user_dates = st_course_presence[course]['user_dates']
-                st_course_presence[course]['percentage_presence'] = percentage_presence(user_dates, course)
-
-        context['st_course_presence'] = st_course_presence
+        context['course_presence'] = course_presence
 
         return context
 
