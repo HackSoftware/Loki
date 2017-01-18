40cdf399bf140757e17992ab4141f99ae9a6e2a7
Slavyana Monkova <slaviana_m@mail.bg>
2017-01-18 15:04:49 +0200

    Delete signals file from education app

--- loki/education/apps.py
+++ loki/education/apps.py
@@ -4,6 +4,3 @@ from django.apps import AppConfig
 class EducationConfig(AppConfig):
     name = 'loki.education'
     verbose_name = 'Education'
-
-    def ready(self):
-        import loki.education.signals  # noqa
