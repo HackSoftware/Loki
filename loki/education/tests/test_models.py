ee0008a3ae8dfae444e76ea911c39983e7bef66f
Slavyana Monkova <slaviana_m@mail.bg>
2017-02-22 16:23:40 +0200

    Adding more tests for WorkingAt model

--- loki/education/tests/test_models.py
+++ loki/education/tests/test_models.py
@@ -1,6 +1,7 @@
 from test_plus.test import TestCase
 from django.core.exceptions import ValidationError
 
+from loki.seed.factories import StudentFactory
 from loki.education.models import WorkingAt
 from loki.base_app.models import Company
 from datetime import datetime
@@ -12,3 +13,14 @@ class WorkingAtTest(TestCase):
         with self.assertRaises(ValidationError):
             WorkingAt.objects.create(start_date=datetime.now().date())
         self.assertEqual(0, WorkingAt.objects.all().count())
+
+    def test_saving_working_at_with_only_company(self):
+        company = Company.objects.create(name="Hack")
+        student = StudentFactory()
+        WorkingAt.objects.create(company=company, student=student, start_date=datetime.now().date())
+        self.assertEqual(1, WorkingAt.objects.all().count())
+
+    def test_saving_working_at_with_company_name(self):
+        student = StudentFactory()
+        WorkingAt.objects.create(company_name="Hack", student=student, start_date=datetime.now().date())
+        self.assertEqual(1, WorkingAt.objects.all().count())
