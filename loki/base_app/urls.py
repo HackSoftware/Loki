063104d1fecb86606c8f47e59fd1555554701468
Ivaylo-Bachvarov <bachvarof@gmail.com>
2017-03-20 13:07:12 +0200

    Delete apis in base_app

--- loki/base_app/urls.py
+++ loki/base_app/urls.py
@@ -1,21 +1,10 @@
 from django.conf.urls import url
 
-from djoser.views import LoginView
-from .views import (
-    me,
-    baseuser_update,
-    base_user_update,
-    user_activation,
-    user_password_reset
-)
+from .views import user_activation, user_password_reset
 
 
 urlpatterns = [
     url(r'^user-activation/(?P<token>[\w-]+)$', user_activation, name="user_activation"),
     url(r'^user-password-reset/(?P<token>[\w-]+)$', user_password_reset,
         name="user_password_reset"),
-    url(r'^api/login/', LoginView.as_view(), name='login'),
-    url(r'^api/me/$', me, name='me'),
-    url(r'^api/baseuser-update', baseuser_update, name='update_baseuser'),
-    url(r'^api/base-user-update/$', base_user_update, name='base_user_update'),
 ]
--- loki/base_app/views.py
+++ loki/base_app/views.py
@@ -10,53 +10,9 @@ from rest_framework.decorators import permission_classes, api_view
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
-from .serializers import BaseUserMeSerializer, UpdateBaseUserSerializer
 from .helper import crop_image, validate_password
 from .models import BaseUserRegisterToken, BaseUserPasswordResetToken, RegisterOrigin
 
-
-@api_view(['GET'])
-@permission_classes((IsAuthenticated,))
-def me(request):
-    logged_user = request.user
-    serializer = BaseUserMeSerializer(logged_user)
-
-    return Response(serializer.data, status=status.HTTP_200_OK)
-
-
-@api_view(['PATCH'])
-@permission_classes((IsAuthenticated,))
-def baseuser_update(request):
-    baseuser = request.user
-    serializer = UpdateBaseUserSerializer(
-        baseuser,
-        data=request.data,
-        partial=True
-    )
-    if serializer.is_valid():
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_200_OK)
-
-    return Response(serializer.errors, status=400)
-
-
-@api_view(['PATCH'])
-@permission_classes((IsAuthenticated,))
-def base_user_update(request):
-    user = request.user
-    co = request.data['selection']
-    co = json.loads(co)
-
-    data = {'full_image': request.data['file']}
-    serializer = UpdateBaseUserSerializer(user, data=data, partial=True)
-    if serializer.is_valid():
-        user = serializer.save()
-        name = crop_image(int(co[0]), int(co[1]), int(co[3]), int(co[2]), str(user.full_image))
-        user.avatar = name
-        user.save()
-        return Response(name, status=status.HTTP_200_OK)
-
-    return Response(serializer.errors, status=400)
 
 
 def user_activation(request, token):
