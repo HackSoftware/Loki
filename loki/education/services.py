b8d6719d1a2f16d2926fc0cd4a781ed91fba4df4
Rositsazz <ross_zz@mail.bg>
2017-03-22 19:29:25 +0200

    Show percent awesome in student box

--- loki/education/services.py
+++ loki/education/services.py
@@ -108,8 +108,9 @@ def get_student_data_for_course(course_assignment):
     percent_awesome = 0
     if total:
         percent_awesome = round((total/all_tasks) * 100, 2)
-    if course_assignment.percent_submitted_solutions != percent_awesome:
-        course_assignment.percent_submitted_solutions = percent_awesome
+    current_percent_submitted_solutions = course_assignment.percent_submitted_solutions
+    if current_percent_submitted_solutions != percent_awesome:
+        current_percent_submitted_solutions = percent_awesome
         course_assignment.save()
 
     course_data['percent_awesome'] = percent_awesome
